a061ce8e83f6d454b12405753bf198ed5c7036f9
Panachai <panachai.ny@gmail.com>
2022-08-29 17:29:01 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// require('dotenv').config()
-import 'dotenv/config'
+import "dotenv/config";
 import Mux from "@mux/mux-node";
+
+console.log("env", {
+  1: process.env.MUX_TOKEN_ID,
+  2: process.env.MUX_TOKEN_SECRET,
+});
 
 const { Video } = new Mux(
   process.env.MUX_TOKEN_ID,
@@ -11,3 +15,5 @@ const asset = await Video.Assets.create({
   input: "https://muxed.s3.amazonaws.com/leds.mp4",
   playback_policy: "public",
 });
+
+console.log("res", { asset, id: asset.id });
